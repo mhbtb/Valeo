32fb6f808756f746555070bfe2aaa932d960c6c2
“mhbtb” <18571571024@163.com>
2020-04-15 10:51:41 +0800

    clrear repository

--- CustomerInterface/G5_AsicFramesLin.h
+++
@@ -1,9 +0,0 @@
-#ifndef G5_ASIC_FRAMES_LIN
-#define G5_ASIC_FRAMES_LIN
-
-#ifdef ASIC_TEST
-extern void CopyRLSDataAsic(void);
-extern void ApplTableInitAsic(void);
-#endif //ASIC_TEST
-
-#endif //G5_ASIC_FRAMES_LIN
--- OS/G5_utilities.h
+++
@@ -1,7 +0,0 @@
-#ifndef G5_UTILITIES_H
-#define G5_UTILITIES_H
-#include "ior5f109be.h"
-
-extern byte wiperSpeed7to2(word currentSpeed0to2In,word speed0to7In, byte hysteresisEdges);
-
-#endif //G5_UTILITIES_H
